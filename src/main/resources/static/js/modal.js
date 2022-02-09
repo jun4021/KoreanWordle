e347bad1c29c297b383fbdcf942b4f1ec5cc6711
박준영 <jun_1204@naver.com>
2022-02-09 23:04:37 +0900

    2/9 남은 시간 추가

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -30,7 +30,7 @@ export function score() {
 function getTime(){
     const now = new Date();
     const hour = 23-now.getHours();
-    const min = 59-now.getMinutes();
+    const min = 60-now.getMinutes();
     const sec = 60-now.getSeconds();
     let timetext = `${hour<10 ? `0${hour}`:hour}:${min<10 ? `0${min}`:min}:${sec<10 ? `0${sec}`:sec}`;
     $("#time").text(timetext);
