b27ad37ef79eb9b9fe65aebc62bff3434a1cd519
jun4021 <86395683+jun4021@users.noreply.github.com>
2022-05-18 15:30:26 +0900

    5/18 일부 수정사항

--- src/main/java/toy/mywordle/controller/MywordleController.java
+++ src/main/java/toy/mywordle/controller/MywordleController.java
@@ -84,7 +84,8 @@ public class MywordleController {
         return "User-agent: *\n" +
                 "Disallow: /admin\n";
     }
-
+    
+    
     @RequestMapping(value={"/sitemap.xml"})
     @ResponseBody
     public String getSiteMap() {
@@ -109,7 +110,8 @@ public class MywordleController {
                 "\n" +
                 "</urlset>";
     }
-
+    
+    
     @GetMapping("/")
     public String home(){
         // DB에서 정답 불러오기
