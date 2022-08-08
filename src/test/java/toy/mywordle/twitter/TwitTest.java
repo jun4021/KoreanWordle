4d22dbfb283e640cdbe67fd5cb3c97a1ae736537
Jun <jun_1204@naver.com>
2022-08-08 17:34:55 +0900

    Twitter Test

---
+++ src/test/java/toy/mywordle/twitter/TwitTest.java
@@ -0,0 +1,12 @@
+package toy.mywordle.twitter;
+
+public class TwitTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void getTweets(String id, String bearerToken) {
+
+    }
+}
