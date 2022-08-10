ffa6ab08a11a0a06747b48ef27891545b07cd3f4
Jun <jun_1204@naver.com>
2022-08-10 23:01:45 +0900

    Twitter Test

--- src/test/java/toy/mywordle/twitter/TwitTest.java
+++ src/test/java/toy/mywordle/twitter/TwitTest.java
@@ -1,9 +1,16 @@
 package toy.mywordle.twitter;
 
+import com.twitter.clientlib.TwitterCredentialsOAuth2;
+import com.twitter.clientlib.api.TwitterApi;
+
 public class TwitTest {
 
     public static void main(String[] args) {
-
+         TwitterCredentialsOAuth2 credentials = new TwitterCredentialsOAuth2("id",
+             "Secret",
+             "TWITTER_OAUTH2_ACCESS_TOKEN",
+             "TWITTER_OAUTH2_REFRESH_TOKEN");
+        TwitterApi api = new TwitterApi(credentials);
     }
 
     private static void getTweets(String id, String bearerToken) {
